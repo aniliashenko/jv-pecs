7a0efe625db502abb616eed917d6936963301d41
Azura <windsweptofficial@gmail.com>
2024-11-02 16:49:20 +0100

    fixed-2

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -36,6 +36,8 @@ public class MachineServiceImpl implements MachineService<Machine> {
 
     @Override
     public void startWorking(List<? extends Machine> machines) {
-
+        for (Machine machine : machines) {
+            machine.doWork();
+        }
     }
 }
